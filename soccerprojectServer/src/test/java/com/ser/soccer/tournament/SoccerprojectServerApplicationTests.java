703d503760f392b194b1a157b32adf507cd004c3
Vinita Maloo <vinitamaloo@yahoo.in>
2021-09-26 16:16:26 -0700

    Initial commit

---
+++ soccerprojectServer/src/test/java/com/ser/soccer/tournament/SoccerprojectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ser.soccer.tournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerprojectServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
