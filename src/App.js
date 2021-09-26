3fe5bdc5ca7f9eedfc0850b2fe853e33b075ba91
Piyush Rewatkar <34715512+piyushrewatkar@users.noreply.github.com>
2021-09-25 20:45:28 -0700

    inital login screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
 import './App.css';
 import './Home/home.js';
-import Home from './Home/home.js';
-import Login from './Login/Register/login';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Login from './Login/login';
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-      <Home/>
-      <Login name="piyush"/>
-    </div>
+    <>
+      
+      <Login />
+    </>
   );
 }
 
