58ddc4d6c89b654c4d3d93d35cf0c158d939db66
JayasaikalyanreddyTummuru <jtummuru@asu.edu>
2021-09-26 23:27:20 -0700

    Added UserProfile crud operations.

--- soccerprojectServer/src/main/java/com/ser/soccer/tournament/userProfiles/UserProfileController.java
+++ soccerprojectServer/src/main/java/com/ser/soccer/tournament/userProfiles/UserProfileController.java
@@ -1,5 +1,30 @@
 package com.ser.soccer.tournament.userProfiles;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userProfileController")
+@RequestMapping("/userProfile/")
 public class UserProfileController {
 
+	@Autowired
+	public UserProfileUsecase userProfileUsecase;
+
+	@GetMapping("get_user_profile/{userId}")
+	public UserProfile getUserProfileById(@PathVariable String userId) {
+		return userProfileUsecase.getUserProfileById(userId);
+	}
+	
+	@PostMapping("add_user_profile")
+	public UserProfile addUserProfile(@RequestBody UserProfile userProfile) {
+		return userProfileUsecase.addUserProfile(userProfile);
+	}
+
 }
+
+ 
--- soccerprojectServer/src/main/java/com/ser/soccer/tournament/userProfiles/UserProfileUsecase.java
+++ soccerprojectServer/src/main/java/com/ser/soccer/tournament/userProfiles/UserProfileUsecase.java
@@ -1,5 +1,25 @@
 package com.ser.soccer.tournament.userProfiles;
 
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("userProfileUsecase")
 public class UserProfileUsecase {
 
+	@Autowired
+	private UserProfileRepo userProfileRepo;
+	
+	public UserProfile getUserProfileById(String userId) {
+		return userProfileRepo.getById(userId);
+	}
+
+	public UserProfile addUserProfile(UserProfile userProfile) {
+		userProfile.setId(UUID.randomUUID().toString());
+		userProfile.setCreatedOn(new Date());
+		userProfile.setModifiedOn(new Date());
+		return userProfileRepo.add(userProfile);
+	}
 }
