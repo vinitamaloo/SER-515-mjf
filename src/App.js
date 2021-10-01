ab3edb96a7dab06d3e9788856bb5513e5352afbc
Piyush Rewatkar <34715512+piyushrewatkar@users.noreply.github.com>
2021-09-30 18:49:32 -0700

    updated login page UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-
-import './App.css';
 import './Home/home.js';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Login from './Login/login';
 import Register from './Register/register';
@@ -8,9 +8,35 @@ import Register from './Register/register';
 
 function App() {
   return (
-    <>
-      <Register />
-    </>
+    <Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          {/* <Link className="navbar-brand" to={"/sign-in"}>xyz</Link> */}
+         
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={Register} />
+          </Switch>
+        </div>
+      </div>
+    </div>
+    </Router>
   );
 }
 
--- src/Login/login.js
+++ src/Login/login.js
@@ -1,6 +1,6 @@
 
 
-
+import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -19,29 +19,29 @@ export default function Login() {
   }
 
   return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-      </Form>
-    </div>
+    <div className="login">
+        <Form onSubmit={handleSubmit}>
+            <h3>Sign In</h3>
+            <Form.Group className="mb-3" controlId="Email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control 
+                    type="email" 
+                    placeholder="Enter Your email" 
+                   />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="Password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control 
+                    type="password" 
+                    placeholder="Enter Your Password"
+                    />
+            </Form.Group>
+            
+            
+            <Button variant="primary" type="submit">Log In</Button>
+            <br/>
+            <span className="form-input-login" >Don't have an account?<Link to={"/sign-up"}>Sign up</Link></span>
+            </Form>
+            </div>
   );
 }
