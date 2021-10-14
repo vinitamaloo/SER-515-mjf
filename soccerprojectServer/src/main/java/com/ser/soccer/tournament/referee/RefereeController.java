4da91189e550bba0e9a55d214ad5bb70b8d7e0d1
Harshita Jain <harshita27apr@gmail.com>
2021-10-14 13:14:13 -0700

    Tested API for referee (Task#37, story#17),

--- soccerprojectServer/src/main/java/com/ser/soccer/tournament/referee/RefereeController.java
+++ soccerprojectServer/src/main/java/com/ser/soccer/tournament/referee/RefereeController.java
@@ -1,6 +1,7 @@
 package com.ser.soccer.tournament.referee;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController("refereeController")
 @RequestMapping("/referee")
 public class RefereeController {
@@ -16,7 +17,7 @@ public class RefereeController {
     @Autowired
 	public  RefereeUsecase refereeUsecase;
 
-    @PostMapping("add_referee")
+    @PostMapping("/add_referee")
 	public Referee addReferee(@RequestBody Referee referee) {
 		return refereeUsecase.addReferee(referee);
 	}
