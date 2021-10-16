8663fad7d2914114305f68ecd9f237a444af07d7
Piyush Rewatkar <34715512+piyushrewatkar@users.noreply.github.com>
2021-10-16 12:50:35 -0700

    task #27 closed

--- client/src/api/services.js
+++ client/src/api/services.js
@@ -4,3 +4,4 @@ const url = 'http://localhost:8080/userProfile/getData';
 
 export const getPostsData = () => { return axios.get("https://jsonplaceholder.typicode.com/posts") };
 export const getData = () => { return axios.get(url)};
+export const getUser = (user) => { return axios.post('http://localhost:8080/admin/getUser', user)};
