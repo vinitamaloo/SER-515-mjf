f5ce8f49255bd907e9d580802b151da2dba8c3b6
Chandra Sekhar Sai Sampath Swaroop Atkuri <catkuri@asu.edu>
2021-10-25 15:58:25 -0700

    updates to the crud operations for team registration

---
+++ soccerprojectServer/src/main/java/com/ser/soccer/tournament/team_registration/TeamRepository.java
@@ -0,0 +1,20 @@
+package com.ser.soccer.tournament.team_registration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository("teamRepo")
+public class TeamRepository {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public TeamRegister add(TeamRegister teamRegister) {
+        if(teamRegister == null)
+            return null;
+
+        return mongoTemplate.save(teamRegister);
+    }
+
+}
+
